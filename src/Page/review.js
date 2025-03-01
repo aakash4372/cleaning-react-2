6567ee29e3477af16adb69adc741b8d80befa0a7
aakash4372 <aakashaakash0013@gmail.com>
2025-03-01 09:54:45 +0530

    code

--- src/Page/review.js
+++ src/Page/review.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './aad.css';
+import './page.css';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
